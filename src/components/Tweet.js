72c539f19f6c442e80988d292ff387b213a3de19
Jason Otter <jasonotter17@gmail.com>
2022-10-10 21:15:18 -0700

    check in before beginning server work

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -1,17 +1,17 @@
 import { Card } from "react-bootstrap";
 import "./Tweet.css";
 
-const Tweet = () => {
+const Tweet = (props) => {
   return (
     <Card className="tweet">
       <h4>This is a tweet.</h4>
       <ul>
-        <li>The full text of the tweet</li>
-        <li>The username of the twitter user who authored the tweet</li>
+        <li>{props.data.username}</li>
+        <li>{props.data.text}</li>
         <li>At least one image (if an image was attached)</li>
         <li>At least one video (if a video was attached)</li>
         <li>At least one gif (if a gif was attached)</li>
-        <li>The retweet count</li>
+        <li>Retweets: {props.data.retweets}</li>
         <li>The “hearted” (favorited) count</li>
       </ul>
     </Card>
--- src/routes/Search.js
+++ src/routes/Search.js
@@ -1,6 +1,49 @@
 import { Fragment } from "react";
 import Tweet from "../components/Tweet";
 
+const tweets = [
+  {
+    username: "@dummyOne",
+    text: "Looking at cleavage is like looking into the sun. You don't stare at it. It's too risky. You get a sense of it, then you look away.",
+    retweets: 15,
+    media: {
+      video: "",
+      image: "",
+      gif: "",
+    },
+  },
+  {
+    username: "@dummyTwo",
+    text: "What could possess anyone to throw a party? I mean, to have a bunch of strangers treat your house like a hotel room.",
+    retweets: 65,
+    media: {
+      video: "",
+      image: "",
+      gif: "",
+    },
+  },
+  {
+    username: "@dummyThree",
+    text: "I feel like my old self again. Totally inadequate, completely insecure, paranoid, neurotic. It's a pleasure.",
+    retweets: 47,
+    media: {
+      video: "",
+      image: "",
+      gif: "",
+    },
+  },
+  {
+    username: "@dummyFour",
+    text: "Yeah, I'm a great quitter. It's one of the few things I do well. I come from a long line of quitters.",
+    retweets: 87,
+    media: {
+      video: "",
+      image: "",
+      gif: "",
+    },
+  },
+];
+
 const Search = () => {
   return (
     <Fragment>
@@ -20,7 +63,9 @@ const Search = () => {
           it look interesting!
         </li>
       </ul>
-      <Tweet />
+      {tweets.map((elem) => (
+        <Tweet key={elem.username} data={elem} />
+      ))}
     </Fragment>
   );
 };
