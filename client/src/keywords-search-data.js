567c12e0f417f6367885998805a28ce073652387
Jason Otter <jasonotter17@gmail.com>
2022-10-20 21:28:39 -0700

    begin work on search, add dummy response data

---
+++ client/src/keywords-search-data.js
@@ -0,0 +1,200 @@
+const fakeResponse = {
+  status: 200,
+  statusTxt: "OK",
+  data: {
+    tweets: [
+      {
+        created_at: "2022-10-21T04:01:31.000Z",
+        text: "RT @PeterSchiff: I agree with @kanyewest. George Floyd died of a fentanyl overdose. That's consistent with the original autopsy, the physic…",
+        public_metrics: {
+          followers_count: 3042,
+          following_count: 4993,
+          tweet_count: 135912,
+          listed_count: 14,
+        },
+        username: "SteveeGeeVee",
+        description: "Musician Writer Artist",
+        name: "Steve Grandinetti",
+        profile_image_url:
+          "https://pbs.twimg.com/profile_images/1440112721001795586/lxJEV2ug_normal.jpg",
+        id: "107335343",
+        verified: false,
+      },
+      {
+        created_at: "2022-10-21T04:01:30.000Z",
+        text: "RT @dolmanlaw: Kanye West faces $250M lawsuit by George Floyd's daughter https://t.co/AaqRdIVhc2 \n#KanyeWest #TRASH",
+        public_metrics: {
+          followers_count: 1190,
+          following_count: 1463,
+          tweet_count: 12556,
+          listed_count: 0,
+        },
+        username: "CasoberR",
+        description:
+          "Rebuilding. 🇺🇦Liberal and proud of it.\nWe need the Oxford comma!\nNo bio = no follow; one word is not a bio.\nWon't even look at DMs. Seriously, don't bother.",
+        name: "CAsober-resistor",
+        profile_image_url:
+          "https://pbs.twimg.com/profile_images/1565847603106828288/_e_zRiZQ_normal.jpg",
+        id: "1565845811811889153",
+        verified: false,
+      },
+      {
+        created_at: "2022-10-21T04:01:10.000Z",
+        text: "RT @TalbertSwan: Many of us have been critical of Kanye, but ain’t no way in Hades we’re gonna sit by and let racist, Blackface wearing, Nw…",
+        public_metrics: {
+          followers_count: 616,
+          following_count: 646,
+          tweet_count: 17194,
+          listed_count: 8,
+        },
+        username: "FBAMEMPHISMAN",
+        description: "FOUNDATIONAL BLACK AMERICAN. \nB1.",
+        name: "Rory",
+        profile_image_url:
+          "https://pbs.twimg.com/profile_images/1574868362773012500/kce6-u88_normal.jpg",
+        id: "1447765352721653763",
+        verified: false,
+      },
+      {
+        created_at: "2022-10-21T04:01:07.000Z",
+        text: "@Rap @kanyewest @elonmusk Cause he is….  ?",
+        public_metrics: {
+          followers_count: 616,
+          following_count: 378,
+          tweet_count: 11288,
+          listed_count: 3,
+        },
+        username: "realdrewV",
+        description: "on the run",
+        name: "Andrew Vollmerhausen",
+        profile_image_url:
+          "https://pbs.twimg.com/profile_images/1462641078943760388/fWnz0rkp_normal.jpg",
+        id: "1072674661",
+        verified: false,
+      },
+      {
+        created_at: "2022-10-21T04:00:55.000Z",
+        text: "RT @PeterSchiff: I agree with @kanyewest. George Floyd died of a fentanyl overdose. That's consistent with the original autopsy, the physic…",
+        public_metrics: {
+          followers_count: 156,
+          following_count: 951,
+          tweet_count: 534,
+          listed_count: 0,
+        },
+        username: "MartinS44652574",
+        description: "Martin Anthony Stephenson",
+        name: "The M A S monstrosity",
+        profile_image_url:
+          "https://pbs.twimg.com/profile_images/1533602326589612033/Cdn7ZMLX_normal.jpg",
+        id: "1532711650398294018",
+        verified: false,
+      },
+      {
+        created_at: "2022-10-21T04:00:51.000Z",
+        text: "@JewishVoice @kanyewest #georgeSoros #traitor https://t.co/l5tsqhkgn0",
+        public_metrics: {
+          followers_count: 52,
+          following_count: 122,
+          tweet_count: 683,
+          listed_count: 0,
+        },
+        username: "PR0FreeSpeech",
+        description: "",
+        name: "C0VlDizStillFake",
+        profile_image_url:
+          "https://pbs.twimg.com/profile_images/1516497251740356608/dVBbSwre_normal.jpg",
+        id: "1516494346979319810",
+        verified: false,
+        media: [
+          {
+            media_key: "3_1583306898832592898",
+            type: "photo",
+            url: "https://pbs.twimg.com/media/FfkJPFtXkAImtsf.png",
+          },
+          {
+            media_key: "3_1583307147445784578",
+            type: "photo",
+            url: "https://pbs.twimg.com/media/FfkJdj3X0AIHwXw.png",
+          },
+          {
+            media_key: "3_1583307147802288133",
+            type: "photo",
+            url: "https://pbs.twimg.com/media/FfkJdlMXoAUpBw3.png",
+          },
+        ],
+      },
+      {
+        created_at: "2022-10-21T04:00:50.000Z",
+        text: "50% OFF! Get your CANDLES NOW! \nMia's Co Lavender Scented Candle https://t.co/yjRsHrveVF\n#hot #instafit #lookwithin #newmedia #lawofattraction\n#kanyewest #kimkardashian #justinbieber #amazonfinds #tiktokmademebuyit #america #usa #canada #losangeles2022-10-21T04:00:50.4691781Z",
+        public_metrics: {
+          followers_count: 10,
+          following_count: 42,
+          tweet_count: 19531,
+          listed_count: 0,
+        },
+        username: "manifestgurutwt",
+        description:
+          "The goal of Manifestation Guru is to bring together a community of like minded people.\nIG: https://t.co/N6ZTU6yaD3",
+        name: "Manifestation Guru",
+        profile_image_url:
+          "https://pbs.twimg.com/profile_images/1429355409551233024/jIGO6t45_normal.jpg",
+        id: "1429355330635399172",
+        verified: false,
+      },
+      {
+        created_at: "2022-10-21T04:00:48.000Z",
+        text: "RT @PeterSchiff: I agree with @kanyewest. George Floyd died of a fentanyl overdose. That's consistent with the original autopsy, the physic…",
+        public_metrics: {
+          followers_count: 802,
+          following_count: 2744,
+          tweet_count: 21083,
+          listed_count: 4,
+        },
+        username: "AmericaEffYeaa",
+        description: "XRP🇺🇲\nQuestion everything.",
+        name: "yep",
+        profile_image_url:
+          "https://pbs.twimg.com/profile_images/1415474747111088133/S5rJ1971_normal.jpg",
+        id: "1120490917310423040",
+        verified: false,
+      },
+      {
+        created_at: "2022-10-21T04:00:35.000Z",
+        text: "RT @fungus_jimmy: These 2 are just too much... here watch the whole thing here---&gt;  https://t.co/3aHOYdrdDi\n\n#KanyeWest #Ye #Kanye #PiersMo…",
+        public_metrics: {
+          followers_count: 1795,
+          following_count: 0,
+          tweet_count: 758181,
+          listed_count: 21,
+        },
+        username: "bungobot1",
+        description:
+          "I like and retweet your tweet, if you follow me. However, sometimes I can't do it all because of the api request limit. ↓If you like the bot, please support me",
+        name: "like and retweet Bot",
+        profile_image_url:
+          "https://pbs.twimg.com/profile_images/1246850684932124672/0dcXfai7_normal.jpg",
+        id: "1246850580191907840",
+        verified: false,
+      },
+      {
+        created_at: "2022-10-21T04:00:35.000Z",
+        text: "RT @JeremyDBoreing: George Floyd's family is apparently suing @kanyewest for $250M over his reaction to @RealCandaceO's documentary, #Great…",
+        public_metrics: {
+          followers_count: 468,
+          following_count: 525,
+          tweet_count: 22598,
+          listed_count: 1,
+        },
+        username: "beamon_ricky",
+        description: "ALOHA FROM HAWAII",
+        name: "Ricky Beamon",
+        profile_image_url:
+          "https://pbs.twimg.com/profile_images/1566729797514002432/IGKcdaNR_normal.jpg",
+        id: "1518994439368318983",
+        verified: false,
+      },
+    ],
+  },
+};
+
+export default fakeResponse;
--- client/src/routes/Search.js
+++ client/src/routes/Search.js
@@ -1,72 +1,30 @@
-import { Fragment } from "react";
-import Tweet from "../components/Tweet";
-
-const tweets = [
-  {
-    username: "@dummyOne",
-    text: "Looking at cleavage is like looking into the sun. You don't stare at it. It's too risky. You get a sense of it, then you look away.",
-    retweets: 15,
-    media: {
-      video: "",
-      image: "",
-      gif: "",
-    },
-  },
-  {
-    username: "@dummyTwo",
-    text: "What could possess anyone to throw a party? I mean, to have a bunch of strangers treat your house like a hotel room.",
-    retweets: 65,
-    media: {
-      video: "",
-      image: "",
-      gif: "",
-    },
-  },
-  {
-    username: "@dummyThree",
-    text: "I feel like my old self again. Totally inadequate, completely insecure, paranoid, neurotic. It's a pleasure.",
-    retweets: 47,
-    media: {
-      video: "",
-      image: "",
-      gif: "",
-    },
-  },
-  {
-    username: "@dummyFour",
-    text: "Yeah, I'm a great quitter. It's one of the few things I do well. I come from a long line of quitters.",
-    retweets: 87,
-    media: {
-      video: "",
-      image: "",
-      gif: "",
-    },
-  },
-];
+import { Container, Row, Col } from "react-bootstrap";
+// import fakeResponse from "../keywords-search-data";
 
 const Search = () => {
   return (
-    <Fragment>
-      <h2>This is the Search page.</h2>
-      <ul>
-        <li>
-          Allows user to search for a twitter username or by tweet content from
-          the last 7 days (if username/content exists then display the 10 most
-          recent tweets)
-        </li>
-        <li>
-          If a user searches for a twitter user that doesn’t exist, they should
-          be notified that the twitter user does not exist
-        </li>
-        <li>
-          The search results that are displayed should look “twitter-like”. Make
-          it look interesting!
-        </li>
-      </ul>
-      {tweets.map((elem) => (
-        <Tweet key={elem.username} data={elem} />
-      ))}
-    </Fragment>
+    <Container>
+      <Row>
+        <Col>
+          <h2>This is the Search page.</h2>
+          <ul>
+            <li>
+              Allows user to search for a twitter username or by tweet content
+              from the last 7 days (if username/content exists then display the
+              10 most recent tweets)
+            </li>
+            <li>
+              If a user searches for a twitter user that doesn’t exist, they
+              should be notified that the twitter user does not exist
+            </li>
+            <li>
+              The search results that are displayed should look “twitter-like”.
+              Make it look interesting!
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
