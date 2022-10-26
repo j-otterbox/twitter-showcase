3a5f1f7f695c5782db1db3449e2304bac04aea17
Jason Otter <jasonotter17@gmail.com>
2022-10-26 10:11:27 -0700

    change loading overlay animation

--- client/src/components/LoadingOverlay.js
+++ client/src/components/LoadingOverlay.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
-import { RevolvingDot } from "react-loader-spinner";
+import { Puff } from "react-loader-spinner";
 import Overlay from "./Overlay";
 
 function LoadingOverlay(props) {
@@ -8,13 +8,12 @@ function LoadingOverlay(props) {
     <Fragment>
       {ReactDOM.createPortal(
         <Overlay isLoading={props.isLoading}>
-          <RevolvingDot
+          <Puff
             height="100"
             width="100"
-            radius="6"
+            radius={1}
             color="#1DA1F2"
-            secondaryColor=""
-            ariaLabel="revolving-dot-loading"
+            ariaLabel="puff-loading"
             wrapperStyle={{}}
             wrapperClass=""
             visible={true}
